cbd205c5519da82c37d05b1a2690b4b9e4ac6428
JackBekket <jackatwork@yandex.ru>
2025-06-04 18:20:08 +0200

    openzeppeline and drafts

---
+++ contracts/drafts/FactoryNFT.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+
+// in-direct imports
+
+import './LLM.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+abstract contract FactoryNFT is Ownable {
+
+
+event CollectionCreated(address indexed creator, address collection);
+
+// from creator to address
+mapping (address => address) public Collections ;
+
+address payable public treasure_fund;
+//address singleton_nft;
+
+constructor() {
+    treasure_fund = payable(msg.sender);
+    //singleton_nft = singleton_collection;
+}
+
+
+
+    /**
+     *  @dev Deploy new ERC721URIStorage contract and return it's address
+     *  @param name_ name of collection
+     *  @param smbl_ symbol of collection
+     *  
+     */
+    function CreateCollection(string memory name_, string memory smbl_, string memory author_,string memory model_name_, uint256 price) public returns (address) {
+        address collection_address = address(new LLM(name_,smbl_,author_,model_name_,price,msg.sender));
+        emit CollectionCreated(msg.sender,collection_address);
+        return collection_address;
+    }
+
+
+}
