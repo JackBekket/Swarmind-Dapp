f1a57cd187bc304f15e7bccf7e0e75e77238dad9
daseinsucks <alexstepov97@gmail.com>
2025-06-20 15:05:10 +0400

    add basic dapp

---
+++ dapp-swarmind/src/pages/DepositPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Deposit from '../components/Deposit';
+
+const DepositPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Deposit Page</h2>
+      <Deposit />
+    </div>
+  );
+};
+
+export default DepositPage;
