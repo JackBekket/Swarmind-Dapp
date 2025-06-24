963f34cadedbc8044a447242ee02ac53601aa7c3
daseinsucks <alexstepov97@gmail.com>
2025-06-24 16:00:19 +0400

    add weth support

--- contracts/credit.sol
+++ contracts/credit.sol
@@ -3,38 +3,41 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
- /// @notice made barebones mint and burn functions, TBU 
 contract Cred is ERC20, Ownable {
-    uint256 public tokensPerEth;
+    IERC20 public weth;
+    uint256 public tokensPerWETH;
 
-    constructor(uint256 initialSupply) ERC20("Swarmind", "SWM") Ownable(msg.sender) {
-        tokensPerEth = 1000 * 10 ** decimals();
+    constructor(address _weth, uint256 initialSupply) ERC20("Swarmind", "SWM") Ownable(msg.sender) {
+        weth = IERC20(_weth);
+        tokensPerWETH = 1000 * 10 ** decimals();
         _mint(msg.sender, initialSupply);
     }
 
-    function decimals() public view override returns (uint8) {
+    function decimals() public pure override returns (uint8) {
         return 9;
     }
 
-    receive() external payable {
-        require(msg.value > 0, "No ETH sent");
-        uint256 tokensToMint = (msg.value * tokensPerEth) / 1 ether;
+    function buy(uint256 wethAmount) external {
+        require(wethAmount > 0, "Zero WETH");
+        require(weth.transferFrom(msg.sender, address(this), wethAmount), "Transfer failed");
+
+        uint256 tokensToMint = (wethAmount * tokensPerWETH) / 1 ether;
         _mint(msg.sender, tokensToMint);
     }
 
-    function burn(uint256 amount) external {
-        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
-        uint256 ethToReturn = (amount * 1 ether) / tokensPerEth;
-        require(address(this).balance >= ethToReturn, "Contract has not enough ETH");
+    function sell(uint256 tokenAmount) external {
+        require(balanceOf(msg.sender) >= tokenAmount, "Not enough tokens");
+        uint256 wethToReturn = (tokenAmount * 1 ether) / tokensPerWETH;
+        require(weth.balanceOf(address(this)) >= wethToReturn, "Contract lacks WETH");
 
-        _burn(msg.sender, amount);
-        payable(msg.sender).transfer(ethToReturn);
+        _burn(msg.sender, tokenAmount);
+        require(weth.transfer(msg.sender, wethToReturn), "Transfer failed");
     }
 
-   
-    function setTokensPerEth(uint256 newRate) external onlyOwner {
+    function setTokensPerWETH(uint256 newRate) external onlyOwner {
         require(newRate > 0, "Rate must be > 0");
-        tokensPerEth = newRate;
+        tokensPerWETH = newRate;
     }
 }
